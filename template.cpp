135da34f7719bb53334b1c875571c21055a750e1
rajprem4214 <rajprem4214@gmail.com>
2022-07-18 19:42:45 +0530

    Added predef functions and code changes

--- template.cpp
+++ template.cpp
@@ -10,10 +10,20 @@ using namespace std;
 #define ss second
 #define PI 3.141592653589793238462
 #define ll long long
-#define fo(i,n) for(i=0;i<n;i++)
+#define all(a) (a).begin(), (a).end()
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rep1(i,a,b) for(ll i=a;i<=b;i++)
+#define nline "\n"
+#define set_bits __builtin_popcount
 typedef vector<int> vi;
 typedef vector<ll> vl;
-
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+// ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 void solve(){
 
@@ -22,10 +32,11 @@ void solve(){
 
 int main() {
 fastio();
-ll t;
-cin>>t;
-while(t--){
-solve();
-}
+   int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
 return 0;
 }
